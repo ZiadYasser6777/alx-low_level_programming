cb89482a902fd96aba30f881c4d4450cb1b7a8ba
ZiadYasser6777 <zzeko620@gmail.com>
2023-03-16 17:36:08 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ *
+ * write(int fd,const void count
+ *
+ *
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful \" - Dora Korpar , 2015-10-19\n";
+
+	write(l , quo, 59);
+	return (1);
+}
