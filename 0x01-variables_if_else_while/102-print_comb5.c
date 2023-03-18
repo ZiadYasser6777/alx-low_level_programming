33942454fac01b2729ad1637fe3ec45a3cac5e0c
ZiadYasser6777 <zzeko620@gmail.com>
2023-03-18 03:10:48 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Description: Write a program that prints all possible
+*
+* different combinations of two digits
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (i == 98 && j == 99)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
