e415e0d1ae962e4d3301c3cdd81f7fe8b7ea50b0
ZiadYasser6777 <zzeko620@gmail.com>
2023-03-17 23:25:47 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	ch = 'A';
+	while (ch <= 'Z')
+	{
+	if (ch == 'q' || ch == 'e')
+	ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar ('\n');
+	return (0);
+}
